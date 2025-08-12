8227be9084423276618647373d989c1fd7f76ad5
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2025-08-12 17:20:12 +0100

    Echo strings

--- scripts/hide_strings.sh
+++ scripts/hide_strings.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
-crowdin string list --verbose | grep -- '--- /no-print ---' | awk '{print $1}' | tr -d '#'
+crowdin string list --verbose | grep -- '--- /no-print ---' | awk '{print $1}' | tr -d '#' > ids.txt
+echo "IDs to hide:"
+cat ids.txt
 
 while read -r id; do
   echo "Hiding string ID: $id"
   crowdin string edit "$id" --hidden
-done
+done < ids.txt
