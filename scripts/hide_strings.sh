f21d2fc03aa75a0a41cf88a88f31f7d405911f6d
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2025-08-12 17:12:22 +0100

    Use verbose flag

--- scripts/hide_strings.sh
+++ scripts/hide_strings.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-crowdin string list | grep -F -- '--- /print-only ---' | awk '{print $1}' | tr -d '#' |
+crowdin string list --verbose | grep -F -- '--- /print-only ---' | awk '{print $1}' | tr -d '#' |
 while read -r id; do
   crowdin string edit "$id" --hidden
 done
